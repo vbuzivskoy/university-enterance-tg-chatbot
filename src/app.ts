9afe195f7812125c490b9b7143fb4e7a4830c7a0
Bohdlesk <bohdanleskovets@gmail.com>
2021-01-27 18:01:42 +0200

    added create user route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 import {connectToDatabase} from "./bd";
+
+const indexRouter = require('./routes/index');
+const createUserRouter = require('./routes/createUser')
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json())
 
-connectToDatabase()
+app.use('/', indexRouter);
+app.use('/api/v1/user', createUserRouter)
+
+// connectToDatabase()
 
 export {
     app
