932ab661b5d290d68fa9faf3bb77e76e635615b7
Bohdlesk <bohdanleskovets@gmail.com>
2021-01-27 18:03:31 +0200

    users interface

---
+++ src/models.ts
@@ -0,0 +1,10 @@
+interface IUser {
+    id: number
+    tgId: string
+    tgUsername: string
+    phoneNumber: string
+    userType: string
+    city: string
+    roleId: number
+    state: string
+}
