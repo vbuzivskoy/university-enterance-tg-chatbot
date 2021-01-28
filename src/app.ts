9727a2ad837f3e22aae665db8c9dfda3217fb94c
Bohdlesk <bohdanleskovets@gmail.com>
2021-01-28 14:58:50 +0200

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser'
+const cors = require('cors');
 import {connectToDatabase} from "./bd";
 
 const indexRouter = require('./routes/index');
@@ -10,8 +11,15 @@ const getAllUsersRouter = require('./routes/getAllUsers')
 
 const app = express();
 
+//cors setup
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
 app.use(express.json());
 app.use(bodyParser.json())
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/api/v1/user', createUserRouter)
