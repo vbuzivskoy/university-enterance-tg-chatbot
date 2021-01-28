b1c857d70e060ceb554a895d59310323418fe32e
Bohdlesk <bohdanleskovets@gmail.com>
2021-01-28 14:25:34 +0200

    delete user route created

---
+++ src/routes/delteteUser.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import {deleteUser} from "../bd";
+
+const router = express.Router();
+
+router.delete('/', (req, res) => {
+    deleteUser(req.params.id)
+        .then(data => {
+            res.status(200).json({
+                status: 'success',
+                info: `user ${req.params.id} has been deleted`
+            })
+        })
+        .catch(error => {
+            res.status(404).json({
+                status: 'error',
+                error
+            })
+        })
+})
+
+module.exports = router;
