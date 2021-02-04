8934b2869e2130d1cec623e3bf23e2f75d3c51f7
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-04 20:47:49 +0200

    added error processing. 'id' parameter changed to 'telegram id'

--- src/routes/createUser.ts
+++ src/routes/createUser.ts
@@ -3,17 +3,22 @@ import {User} from "../models";
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    User.create(req.body)
-        .then(data => {
-            res.status(200).json({
-                status: 'success',
-                user: data
-            })
+router.post('/', async (req, res) => {
+    try {
+        if (req.body.tg_id === undefined) {
+            throw new Error('Telegram id (tg_id) parameter is not found');
+        }
+        const user = await User.create(req.body);
+        res.status(200).json({
+            status: 'success',
+            user
         })
-        .catch(err => {
-            return err
+    } catch (error) {
+        res.status(404).json({
+            status: 'error',
+            error: error
         })
+    }
 })
 
 module.exports = router;
