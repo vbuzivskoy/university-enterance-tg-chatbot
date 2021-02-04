2d6f8158f2b817ad6114ab4b0e6f08dfa1e5988b
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-04 15:53:17 +0200

    require was changed to import. added new routes. changed connection to DB

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser'
-const cors = require('cors');
-import {connectToDatabase} from "./bd";
+import cors from 'cors'
+import {connectToDB} from "./bd";
 
 const indexRouter = require('./routes/index');
 const createUserRouter = require('./routes/createUser')
 const deleteUserRouter = require('./routes/delteteUser')
 const getUserRouter = require('./routes/getUserById')
 const getAllUsersRouter = require('./routes/getAllUsers')
+const changeUserRouter = require('./routes/updateUser')
 
 const app = express();
 
@@ -26,8 +27,9 @@ app.use('/api/v1/user', createUserRouter)
 app.use('/api/v1/user', getAllUsersRouter)
 app.use('/api/v1/user', getUserRouter)
 app.use('/api/v1/user', deleteUserRouter)
+app.use('/api/v1/user', changeUserRouter)
 
-connectToDatabase()
+connectToDB()
 
 export {
     app
