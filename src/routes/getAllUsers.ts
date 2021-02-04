53d24f063a17e9006fc78d5705126ff391c1dc80
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-04 21:03:59 +0200

    changed to async - await

--- src/routes/getAllUsers.ts
+++ src/routes/getAllUsers.ts
@@ -4,21 +4,19 @@ import {User} from "../models";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    User.findAll({})
-        .then(users => {
-            console.log("All users:", JSON.stringify(users, null, 2));
-            res.status(200).json({
-                status: 'success',
-                users
-            })
+router.get('/', async (req, res) => {
+    try{
+        const usersList = await User.findAll()
+        res.status(200).json({
+            status: 'success',
+            users: usersList
         })
-        .catch(error => {
-            res.status(404).json({
-                status: 'error',
-                error
-            })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message,
+            error: error
         })
+    }
 })
 
 module.exports = router;
