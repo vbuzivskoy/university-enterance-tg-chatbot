7bb711e12fef9f2f075a0bc7dca419db7c048f0a
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-05 12:37:19 +0200

    env variable data type error fixed

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+declare var process : {
+    env: {
+        DATABASE_URL: string
+    }
+}
+
 const conString = process.env.DATABASE_URL;
 
-export {conString};
+export { conString };
