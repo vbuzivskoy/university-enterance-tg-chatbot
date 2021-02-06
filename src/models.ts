2a1d9f354340177e2055923c044eeefdfe670117
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-06 13:22:59 +0200

    database tables cosmetic changes

--- src/models.ts
+++ src/models.ts
@@ -1,17 +1,17 @@
 import {
-    Sequelize,
-    Model,
-    ModelDefined,
-    DataTypes,
-    HasManyGetAssociationsMixin,
-    HasManyAddAssociationMixin,
-    HasManyHasAssociationMixin,
-    Association,
-    HasManyCountAssociationsMixin,
-    HasManyCreateAssociationMixin,
-    Optional,
-} from "sequelize";
-import {db} from "./bd";
+  Sequelize,
+  Model,
+  ModelDefined,
+  DataTypes,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  Association,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  Optional,
+} from 'sequelize';
+import { db } from './bd';
 
 interface IUser {
     id?: number
@@ -31,112 +31,111 @@ enum userType {
 }
 
 const UserType = db.define('user_types', {
-        type_name: {
-            type: DataTypes.STRING
-        }
-    },
-    {
-        timestamps: false
-    }
-)
+  type_name: {
+    type: DataTypes.STRING,
+  },
+},
+{
+  timestamps: false,
+});
 
 const UserRole = db.define('user_roles', {
-        role_name: {
-            type: DataTypes.STRING
-        }
-    },
-    {
-        timestamps: false
-    }
-)
+  role_name: {
+    type: DataTypes.STRING,
+  },
+},
+{
+  timestamps: false,
+});
 
 const BotSetting = db.define('bot_settings', {
-        name: {
-            type: DataTypes.STRING
-        },
-        value: {
-            type: DataTypes.STRING
-        }
-    },
-    {
-        timestamps: false
-    }
-)
+  name: {
+    type: DataTypes.STRING,
+  },
+  value: {
+    type: DataTypes.STRING,
+  },
+},
+{
+  timestamps: false,
+});
 
 const BotFAQ = db.define('bot_faq', {
-        name: {
-            type: DataTypes.STRING
-        },
-        text: {
-            type: DataTypes.STRING(2000)
-        }
-    },
-    {
-        timestamps: false
-    }
-)
+  name: {
+    type: DataTypes.STRING,
+  },
+  text: {
+    type: DataTypes.STRING(2000),
+  },
+},
+{
+  timestamps: false,
+});
 
 const Question = db.define('questions', {
-    question: {
-        type: DataTypes.STRING(2000)
-    },
-    answer: {
-        type: DataTypes.STRING(2000)
-    },
-    has_answer: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    },
-    stats: {
-        type: DataTypes.BIGINT,
-        defaultValue: 1
-    }
-})
+  question: {
+    type: DataTypes.STRING(2000),
+    allowNull: false,
+  },
+  answer: {
+    type: DataTypes.STRING(2000),
+  },
+  has_answer: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  stats: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+});
 
 const User = db.define('users', {
-        tg_id: {
-            type: DataTypes.STRING,
-            unique: true
-        },
-        tg_name: {
-            type: DataTypes.STRING,
-        },
-        name: {
-            type: DataTypes.STRING
-        },
-        phone_number: {
-            type: DataTypes.STRING,
-            unique: true
-        },
-        type_id: {
-            type: DataTypes.BIGINT,
-            defaultValue: 1,
+  tg_id: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  tg_name: {
+    type: DataTypes.STRING,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  type_id: {
+    type: DataTypes.BIGINT,
+    defaultValue: 1,
 
-            references: {
-                model: UserType,
-                key: 'id',
-            }
-        },
-        role_id: {
-            type: DataTypes.BIGINT,
-
-            references: {
-                model: UserRole,
-                key: 'id',
-            }
-        },
-        city: {
-            type: DataTypes.STRING
-        },
-        state: {
-            type: DataTypes.JSON
-        }
+    references: {
+      model: UserType,
+      key: 'id',
     },
-    {
-        timestamps: false
-    }
-)
+  },
+  role_id: {
+    type: DataTypes.BIGINT,
+
+    references: {
+      model: UserRole,
+      key: 'id',
+    },
+  },
+  city: {
+    type: DataTypes.STRING,
+  },
+  state: {
+    type: DataTypes.JSON,
+  },
+},
+{
+  timestamps: false,
+});
 
 // User.sync({force: true})
+// Question.sync({force: true})
 
-export {UserRole, UserType, User, IUser, BotFAQ, BotSetting, Question}
+export {
+  UserRole, UserType, User, IUser, BotFAQ, BotSetting, Question,
+};
