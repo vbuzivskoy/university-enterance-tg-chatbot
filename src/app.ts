49b0ac98758651799f8b9e8cb6307ec400ddad4e
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-06 13:15:50 +0200

    routers require changed to import

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,40 @@
 import express from 'express';
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import {connectToDB} from "./bd";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { connectToDB } from './bd';
 
-const indexRouter = require('./routes/index');
-const createUserRouter = require('./routes/createUser')
-const deleteUserRouter = require('./routes/delteteUser')
-const getUserRouter = require('./routes/getUserById')
-const getAllUsersRouter = require('./routes/getAllUsers')
-const changeUserRouter = require('./routes/updateUser')
-
-const getQuestionRoute = require('./routes/questions/test')
-const createQuestionRoute = require('./routes/questions/createQuestion')
+// router imports
+import { getAllUsersRouter } from './routes/getAllUsers';
+import { createUserRouter } from './routes/createUser';
+import { deleteUserRouter } from './routes/delteteUser';
+import { getUserByTelegramIdRouter } from './routes/getUserById';
+import { updateUserDataRouter } from './routes/updateUser';
+import { addAdminRouter } from './routes/admin routes/addAdmin';
+import { deleteAdminRouter } from './routes/admin routes/deleteAdmin';
+import { createQuestionRouter } from './routes/questions routes/createQuestion';
+import { getQuestionsListRouter } from './routes/questions routes/getQuestionsList';
+import { adminsListRouter } from './routes/admin routes/getAminsList';
+import { getQuestionByIdRouter } from './routes/questions routes/getQuestionById';
+import { getPopularQuestionsListRouter } from './routes/questions routes/getPopularQuestions';
+import { changeFAQRouter } from './routes/FAQ change routes/changeFAQ';
 
 const app = express();
 
-//cors setup
+// cors setup
 const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-}
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
-app.use('/', indexRouter);
-app.use('/api/v1/user', createUserRouter)
-app.use('/api/v1/user', getAllUsersRouter)
-app.use('/api/v1/user', getUserRouter)
-app.use('/api/v1/user', deleteUserRouter)
-app.use('/api/v1/user', changeUserRouter)
+app.use('/api/v1/users', createUserRouter);
+app.use('/api/v1/users', getAllUsersRouter);
+app.use('/api/v1/users', deleteUserRouter);
+app.use('/api/v1/users', getUserByTelegramIdRouter);
+app.use('/api/v1/users', updateUserDataRouter);
 
 app.use('/api/v1/question', getQuestionRoute)
 app.use('/api/v1/question', createQuestionRoute)
@@ -39,5 +43,5 @@ app.use('/api/v1/question', createQuestionRoute)
 connectToDB()
 
 export {
-    app
-}
+  app,
+};
