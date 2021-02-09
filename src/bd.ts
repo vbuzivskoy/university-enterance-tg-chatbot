5230fb7c090329faad1f90dbf57ce81a684fad57
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-09 20:10:30 +0200

    deleted old data

--- src/bd.ts
+++ src/bd.ts
@@ -1,6 +1,4 @@
-import pg from 'pg';
 import { Sequelize } from 'sequelize';
-import { IUser } from './models';
 import { conString } from './const';
 
 const db = new Sequelize(
@@ -16,16 +14,6 @@ const db = new Sequelize(
   },
 );
 
-function synchronizeDataBase() {
-  db.sync({ alter: true })
-    .then(() => {
-      console.log('All models were synchronized successfully.');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
 async function connectToDB() {
   try {
     await db.authenticate();
@@ -35,49 +23,6 @@ async function connectToDB() {
   }
 }
 
-// db connect settings
-pg.defaults.ssl = true;
-const client = new pg.Client({
-  connectionString: conString,
-  ssl: { rejectUnauthorized: false },
-});
-
-function connectToDatabase(): void {
-  client.connect()
-    .then(() => client
-      .query('SELECT NOW() AS "theTime"'))
-    .then((data: any): void => {
-      console.log('DB connected ', data.rows[0].theTime);
-    })
-    .catch((err: any) => {
-      console.error('Error: ', err);
-    });
-}
-
-function createUser(routeData: IUser): Promise<any> {
-  return client
-    .query(`INSERT INTO public.users (tg_id, tg_username, pnohe_number, user_type, city, type_id, state)
-                VALUES ($1, $2, $3, $4, $5, $6, $7)
-                RETURNING *`,
-    [routeData.tgId, routeData.tgUsername, routeData.phoneNumber, routeData.userType,
-      routeData.city, routeData.roleId, routeData.state]);
-}
-
-function deleteUser(userId: number | string): Promise<any> {
-  return client
-    .query('DELETE FROM users WHERE id = $1', [userId]);
-}
-
-function getUser(userId: number | string): Promise<any> {
-  return client
-    .query('SELECT * FROM users WHERE id = $1', [userId]);
-}
-
-function getAllUsers(): Promise<any> {
-  return client
-    .query('SELECT * FROM users');
-}
-
 export {
-  connectToDatabase, createUser, deleteUser, getUser, getAllUsers, connectToDB, db, synchronizeDataBase,
+    connectToDB, db
 };
