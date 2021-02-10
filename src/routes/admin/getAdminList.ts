85741a53f102893eea69e7c0380af0f815bfa687
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-10 13:07:10 +0200

    change folder of addAdmin router. rebuild structure

---
+++ src/routes/admin/getAdminList.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { User } from '../../models';
+
+const adminsListRouter = express.Router();
+
+adminsListRouter.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        type_id: 2,
+      },
+    });
+    res.status(200).json({
+      status: 'success',
+      users,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+      error,
+    });
+  }
+});
+
+export { adminsListRouter };
--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import {addAdminRouter} from "./addAdmin";
+import { addAdminRouter } from './addAdmin';
+import { deleteAdminRouter } from './deleteAdmin';
+import { adminsListRouter } from './getAdminList';
 
 const adminsRouter = Router();
 
-adminsRouter.use('/add', addAdminRouter)
+adminsRouter.use('/add', addAdminRouter);
+deleteAdminRouter.use('/delete', addAdminRouter);
+adminsListRouter.use('/get', addAdminRouter);
 
-export { adminsRouter }
+export { adminsRouter };
