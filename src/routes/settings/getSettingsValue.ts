a3c570ffe3e4238ee10cca4d38bca0f36cbf9d77
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-11 21:01:36 +0200

    added ability to get bot settings values

---
+++ src/routes/settings/getSettingsValue.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import { BotSetting } from '../../models';
+
+const getSettingsValueRouter = express.Router();
+
+getSettingsValueRouter.get('/', async (req, res) => {
+  try {
+    let params = {};
+    if (req.query.name) {
+      params = {
+        where: {
+          name: req.query.name,
+        },
+      };
+    }
+    const settingsValue = await BotSetting.findAll(params);
+    if (settingsValue.length === 0) {
+      throw new Error('Not found');
+    }
+    res.status(200).json({
+      status: 'success',
+      settings: settingsValue,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      error,
+    });
+  }
+});
+
+export { getSettingsValueRouter };
--- src/routes/settings/index.ts
+++ src/routes/settings/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { changeSettingsRouter } from './changeSettings';
+import { getSettingsValueRouter } from './getSettingsValue';
 
 const settingsRouter = Router();
 
 settingsRouter.use('/change', changeSettingsRouter);
+settingsRouter.use('/get', getSettingsValueRouter);
 
 export { settingsRouter };
